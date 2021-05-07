a6956fbc35d01d788b1aac3bab9acfdbeea942c6
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-08 01:35:10 +1000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Automated Technical Solutions',
+  tagline: 'We solve problems and teach you how to',
+  url: 'https://automatedtechnicalsolutions.com',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Automated Tech Solutions',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -22,7 +22,7 @@ module.exports = {
         },
         {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/lreynolds188',
           label: 'GitHub',
           position: 'right',
         },
