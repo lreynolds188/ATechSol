caa95f1c9ffbae0a0951eba8662a254dffefe83c
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-08 21:08:54 +1000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,5 @@
 module.exports = {
   someSidebar: {
     Guides: ['Welcome', 'Nextcloud Server Ubuntu 20.04', 'GrapheneOS Pixel'],
-    Legal: ['Terms of Service', 'Privacy Policy', 'Disclaimer'],
   },
 };
