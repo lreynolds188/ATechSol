aa8c0645b7b5424120cdf16785b2955c6f7e50ea
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-08 23:22:32 +1000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,8 @@
 module.exports = {
   someSidebar: {
     Introduction: ['Welcome'],
-    Open_Source_Software: ['Kubuntu', 'NextCloud', 'GrapheneOS', 'InvoiceNinja'],
-    Recommended_Providers: ['DreamHost', 'Cloudflare'],
+    Open_Source_Software: ['Kubuntu', 'Nextcloud', 'GrapheneOS', 'InvoiceNinja'],
+    Recommended_Providers: ['GitHub', 'Vercel', 'DreamHost', 'Cloudflare'],
     Guides: ['Nextcloud Server Ubuntu 20.04', 'GrapheneOS Pixel'],
     Our_Services: ['Automation', 'Implementation', 'Information', 'Support'],
   },
