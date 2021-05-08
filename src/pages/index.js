435ab58acb1879ba27709088172599e7f299758c
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-08 15:23:33 +1000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const features = [
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        My goal is to provide the best solutions to a range of technical and operating issues as well as a guided explanation that will help you understand and resolve these issues for yourself and your team in the future.
+        Our goal is to provide the best solutions to a range of technical and operating issues as well as a guided explanation that will help you understand and resolve these issues for yourself and your team in the future.
       </>
     ),
   },
