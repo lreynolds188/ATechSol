98773bcb01d961667a7e8973df400322712657b4
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-17 14:12:47 +1000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,13 @@
 module.exports = {
-  someSidebar: {
+  docs: {
     Introduction: ['Welcome'],
     Open_Source_Software: ['Kubuntu', 'Nextcloud', 'GrapheneOS', 'LibreOffice', 'Joplin', 'InvoiceNinja'],
     Service_Providers: ['DreamHost', 'Cloudflare', 'GitHub', 'Vercel'],
     Our_Services: ['Implementation', 'Information', 'Automation'],
     Our_Cause: ['Donate']
+  },
+  blog: {
+    Introduction: ['Welcome'],
+    Guides: ['NextcloudServer'],
   },
 };
