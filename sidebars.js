45729fc2eeed1b8ed9dde6e2f34ade4701d05542
Luke Reynolds <17304215+lreynolds188@users.noreply.github.com>
2021-05-21 16:21:38 +1000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -4,12 +4,53 @@ module.exports = {
     {
       type: 'category',
       label: 'Open Source Software',
-      items: ['Kubuntu', 'GrapheneOS', 'Brave', 'LibreOffice', 'Nextcloud', 'Joplin', 'InvoiceNinja'],
+      items: [
+        'open-source-software/desktop-os',
+        {
+          'Desktop OS': [
+            'desktop-os/Kubuntu', 
+            'desktop-os/GrapheneOS', 
+            'desktop-os/UbuntuTouch', 
+            'desktop-os/Brave', 
+            'desktop-os/LibreOffice', 
+            'desktop-os/Nextcloud', 
+            'desktop-os/Joplin', 
+            'desktop-os/InvoiceNinja',
+          ],
+          'Mobile OS': [
+            'mobile-os/GrapheneOS', 
+            'mobile-os/UbuntuTouch',
+          ],
+          'Web Browser': [
+            'web-browser/Brave',
+          ],
+          'Cloud': [
+            'cloud/Nextcloud',
+          ],
+          'Office': [
+            'office/LibreOffice', 
+            'office/Joplin', 
+            'office/InvoiceNinja',
+          ],
+          '3D Printing': [
+            '3d-printing/Tinkercad', 
+            '3d-printing/Cura',
+          ],
+        },
     },
     {
       type: 'category',
       label: 'Service Providers',
       items: ['DreamHost', 'Cloudflare', 'GitHub', 'Vercel'],
+    },
+    {
+      type: 'category',
+      label: 'Cryptocurrency',
+      items: [
+        'Exchange': [
+          'exchange/Binance', 
+          'exchange/Coinbase',
+      ],
     },
     {
       type: 'category',
